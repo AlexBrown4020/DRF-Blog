5280ff27038eec57eb1f60cd93e619c876290115
AlexBrown4020 <balex3497@gmail.com>
2022-08-08 14:47:28 +0900

    Feat: add managers to Post fields

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,11 @@ class Category(models.Model):
         return self.name
 
 class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
     category = models.ForeignKey(
         Category, on_delete=models.PROTECT, default=1)
     title = models.CharField(max_length=250)
@@ -20,3 +25,11 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='blog_posts')
     status = models.CharField(
         max_length=10, choices=options, default='published')
+    objects = models.Manager()
+    postobjects = PostObjects()
+
+    class Meta:
+        ordering = ('-published',)
+
+        def __str__(self):
+            return self.title
