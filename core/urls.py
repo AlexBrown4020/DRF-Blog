44759313189d07189cac565e597cd0ad347e55a0
AlexBrown4020 <balex3497@gmail.com>
2022-08-08 14:05:14 +0900

    Feat: index page connected to settings

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogs.urls', namespace='blog')),
-    path('api', include('blog_api_urls', namespace='blog_api')),
+    path('', include('blog.urls', namespace='blog')),
+    # path('api', include('blog_api_urls', namespace='blog_api')),
 ]
