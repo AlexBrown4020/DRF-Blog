50208bbf0a7dc62200aee84f969a2fd40315db15
AlexBrown4020 <balex3497@gmail.com>
2022-08-09 13:35:59 +0900

    Feat: Alter test content

--- blog/tests.py
+++ blog/tests.py
@@ -8,7 +8,7 @@ class TestCreatePost(TestCase):
         test_category = Category.objects.create(name='Flask')
         test_user = User.objects.create_user(
             username='test_user', password='1234')
-        test_post = Post.objects.create(category_id=1, title='Post Title', excerpt='excerpt', content='content', slug='post-title', author_id=1, status='published')
+        test_post = Post.objects.create(category_id=1, title='Post Title', excerpt='excerpt', content='posted content', slug='post-title', author_id=1, status='published')
 
     def test_blog_content(self):
         post = Post.postobjects.get(id=1)
@@ -21,7 +21,7 @@ class TestCreatePost(TestCase):
         self.assertEqual(author, 'test_user')
         self.assertEqual(title, 'Post Title')
         self.assertEqual(excerpt, 'excerpt')
-        self.assertEqual(content, 'content')
+        self.assertEqual(content, 'posted content')
         self.assertEqual(status, 'published')
         self.assertEqual(str(post), 'Post Title')
         self.assertEqual(str(cat), 'Flask')
